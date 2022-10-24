9456a609f044ea2f8ae74dc153f06d4faa7bc06f
vale quiroga <valentinqramos@outlook.com>
2022-10-24 14:42:30 -0500

    fixing filter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import { Component } from "react";
 import "./App.css";
 
+// Monsters Rolodex - Storing Original Data
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
+    console.log("constructor");
   }
-  componentDidMount(){
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(users => this.setState(() => {
-        return {monsters: users}
-      }))
-      .catch(error => console.log(error)) 
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      )
+      .catch((error) => console.log(error));
+    console.log("componentDidMount");
   }
 
   render() {
+    console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1> {monster.name} </h1>
